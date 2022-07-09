b1937f928c9261438e6a1bdf6a3bc39f0bd9a600
amish_76 <90342175+amish0301@users.noreply.github.com>
2022-07-09 22:45:06 +0530

    Update MinEl.cpp

--- MinEl.cpp
+++ MinEl.cpp
@@ -1,21 +1,14 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main(){
-    int nums[7] = {10, 20, 30, 40, 50, 5, 7};
-
-    int s = 0, e = 6, ans = INT_MAX;
-    while(s <= e)
-    {
-        int mid = s + (e - s) / 2;
-
-        ans = min(ans, nums[mid]);
-
-        if(nums[mid] <= nums[s])
-            e = mid - 1;
-        else
-            s = mid + 1;
+int findMin(int nums[], int n){
+        int s = 0 , e = n - 1;
+        
+        if(nums[s] < nums[e]) return nums[s];
+        
+        while(s < e)
+        {
+            int mid = s + (e-s)/2;
+            
+            if(nums[mid] >= nums[0]) s = mid + 1;
+            else e = mid;
+        }
+        return nums[s];
     }
-    cout << ans;
-    return 0;
-}
